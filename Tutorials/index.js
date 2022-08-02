0c6728f3a482bdeaddc79e2d456dcef87b6b6990
UNDP Guest Account <undp@undp.org>
2022-08-02 14:57:58 +0200

    inspekti 1

--- Tutorials/index.js
+++ Tutorials/index.js
@@ -1,7 +1,8 @@
 alert("hey")
 let js = 'amazing'
-if (js === 'amazing') alert('bye')
-js = 'boring'
-if (js === 'amazing') alert('bye')
-40+8+23-10
-lc61
+if (js === 'amazing'){
+    alert(js)
+}else{
+    alert("not "+js)
+}
+
